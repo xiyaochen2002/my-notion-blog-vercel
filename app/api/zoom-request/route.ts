112e4013e81e52bb4aac6f9d16bb597678a4d899
xiyaochen2002 <12112912@mail.sustech.edu.cn>
2026-04-14 15:27:30 -0500

    fix: move api routes to correct folder and update logic

--- app/api/zoom-request/route.ts
+++ app/api/zoom-request/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import { Resend } from "resend";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend("re_XRSYiMse_GbmG8yukqZkG7WkmLRq1odAG");
 
 export async function POST(req: NextRequest) {
   try {
